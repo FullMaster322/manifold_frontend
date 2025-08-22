d999e58438bdbeb24dd0319994db07490248492b
zahar <zmajmeskul@gmail.com>
2025-08-22 04:03:40 +0300

    1.555

--- manifold/src/App.vue
+++ manifold/src/App.vue
@@ -65,28 +65,36 @@ export default {
 
 
 <template>
-
-  <div class="loginPanelBack" :style="{ display: hideStatus }">
-    <div class="loginPanel">
+  <link rel="stylesheet" href="./assets/app.css">
+  <div class="overlay-login" :style="{ display: hideStatus }">
+    <div class="panel-login">
+      <div class="panel-header">
+        <span class="btn-close" @click="changeHideStatus">close</span>
+      </div>
       <h1>Login</h1>
-      <input />
+      <div class="form-login">
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <div>
+          <button class="btn-login">Login</button>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="navbar">
-    <div class="navbarButtons">
 
-    <nav>
-      <span :class="{ active: activeSection === 'home' }" @click="toHome" style="margin-left: 150px;">Home</span>
-      <span :class="{ active: activeSection === 'messenger' }" @click="toMessenger" style="color: #00bfff;">Start messenger</span>
-      <span :class="{ active: activeSection === 'list' }" @click="toList">AI list</span>
-      <span :class="{ active: activeSection === 'about' }" @click="toAbout">About</span>
-      <span :class="{ active: activeSection === 'contacts' }" @click="toContacts">Contacts</span>
-      <span style="margin-left: 100px;" @click="changeHideStatus">Login / Registration</span>
-        
-    </nav>
-
+  <div class="header-navbar">
+    <div class="navbar-content">
+      <nav>
+        <span :class="{ active: activeSection === 'home' }" @click="toHome" class="nav-item" style="margin-left: 150px;">Home</span>
+        <span :class="{ active: activeSection === 'messenger' }" @click="toMessenger" class="nav-item" style="color: #00bfff;">Start messenger</span>
+        <span :class="{ active: activeSection === 'list' }" @click="toList" class="nav-item">AI list</span>
+        <span :class="{ active: activeSection === 'about' }" @click="toAbout" class="nav-item">About</span>
+        <span :class="{ active: activeSection === 'contacts' }" @click="toContacts" class="nav-item">Contacts</span>
+        <span class="nav-item" style="margin-left: 100px;" @click="changeHideStatus">Login / Registration</span>
+      </nav>
     </div>
   </div>
+
   <RouterView />
 </template>
 
@@ -101,52 +109,130 @@ body {
   color: #e0e0e0;
   overflow-y: auto;
 }
-.loginPanelBack {
-  background: rgba(255, 255, 255, 0.066);
+
+.form-login {
+  background-color: transparent;
+  height: 50px;
+}
+
+.form-login input {
+  width: 65%;
+  background-color: #2a2a2a;
+  border: 1px solid #2a2a2a;
+  box-shadow: 0 0 8px #2a2a2a;
+  color: white;
+  font-size: 18px;
+  border-radius: 10px;
+  padding: 12px 25px;
+  margin: 10px;
+  resize: none;
+  outline: none;
+}
+
+.form-login input:focus {
+  background-color: #333;
+}
+
+.form-login input::placeholder {
+  color: rgb(221, 221, 221);
+  font-size: 18px;
+}
+
+.btn-close {
+  background: none;
+  border: none;
+  color: #aaa;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: color 0.2s ease;
+}
+
+.btn-close:hover {
+  color: #fff;
+}
+
+.overlay-login {
+  background: rgba(0, 0, 0, 0.81);
   backdrop-filter: blur(8px);
-  position: absolute; width: 100%;  height: 100%; text-align: center; z-index: 9999;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  z-index: 9999;
   border: 1px solid rgba(255, 255, 255, 0.066);
   border-radius: 20px;
 }
-.loginPanel {
-  margin-top: 10%;
-  background: rgb(20, 25, 35); 
-  margin-left: 35%; 
-  margin-right: 35%;
-  border: 1px;
-  border-radius: 10px;
+
+.panel-login {
+  margin: 10% auto;
+  background-color: #1f1f1f;
+  width: 40%;
+  border-radius: 16px;
+  border: 1px solid #444;
+
+  box-shadow: 0 0 20px rgba(0, 191, 255, 0.2);
+  display: flex;
+  flex-direction: column;
+  padding-top: 0;
+  padding-bottom: 250px;
 }
-.navbar {
+
+.panel-header {
+  text-align: right;
+  justify-content: end;
+  align-items: end;
+}
+
+.btn-login {
+  background-color: #003d79;
+  color: white;
+  width: 200px;
+  border: none;
+  padding: 12px;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  margin-top: 50px;
+}
+
+.btn-login:hover {
+  background-color: #005b82;
+}
+
+.header-navbar {
   width: 100%;
   height: 50px;
   background: rgba(20, 25, 35, 0.6);
   backdrop-filter: blur(14px);
   border-bottom: 1px solid rgba(255, 255, 255, 0.05);
-  margin: 0;
-  text-align: center;
   position: fixed;
   z-index: 9999;
-  
+  text-align: center;
 }
-.navbarButtons {
+
+.navbar-content {
   padding-top: 14px;
 }
-.navbarButtons span {
+
+.nav-item {
   margin-left: 30px;
   cursor: pointer;
   color: rgb(212, 212, 212);
   transition: 0.2s;
   font-size: 16px;
   width: 80px;
-  
 }
-.navbarButtons span:hover {
+
+.nav-item:hover {
   color: white;
 }
-.navbarButtons span.active {
+
+.nav-item.active {
   color: #00bfff;
   border-bottom: 1px solid #00bfff;
 }
+
 .active {
   border-bottom: 1px solid #00bfff;
   color: #00bfff;
